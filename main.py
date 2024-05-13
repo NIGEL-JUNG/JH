6e3e0a45a80aea12a683a03b92adba18ee8682b6
MINGW64 <jjh010658@gnu.ac.kr>
2024-05-13 19:39:51 +0900

    하이루

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from datetime import datetime
+
 
 app = FastAPI()
 
@@ -15,5 +15,5 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/users/{user_name}")
-async def welcome():
-    return templates.TemplateResponse()
+async def root(request: Request):
+    return templates.TemplateResponse(request=request, name="2.html")
